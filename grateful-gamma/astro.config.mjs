efcf1a488ae161b2e3ebad62e0b037157ca6c212
Strahinja Becagol <becagolwork@gmail.com>
2025-07-25 22:31:22 +0200

    chore: update GitHub Pages deployment workflow and add static output config

--- grateful-gamma/astro.config.mjs
+++ grateful-gamma/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://strvstraja.github.io',
   base: '/sbecagoldotcom',
+  output: 'static',
   build: {
     assets: 'assets'
   }
