0786ff20c1cdd0ec2297f825c7c9b9dea1d2c118
Strahinja Becagol <becagolwork@gmail.com>
2025-07-25 22:14:51 +0200

    Update Astro config with correct GitHub username and repository

--- grateful-gamma/astro.config.mjs
+++ grateful-gamma/astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sbecagol.github.io',
-  base: '/grateful-gamma',
+  site: 'https://strvstraja.github.io',
+  base: '/sbecagoldotcom',
   build: {
     assets: 'assets'
   }
