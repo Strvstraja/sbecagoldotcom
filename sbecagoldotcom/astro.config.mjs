415b2e00602a6d160722ad3eeed02516f48e36e2
Strahinja Becagol <becagolwork@gmail.com>
2025-07-26 10:09:58 +0200

    Fix Astro deploy to GitHub Pages

--- sbecagoldotcom/astro.config.mjs
+++ sbecagoldotcom/astro.config.mjs
@@ -1,12 +1,8 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://strvstraja.github.io',
-  base: '/sbecagoldotcom',
   build: {
-    assets: 'assets'
-  }
+    outDir: './dist',
+  },
 });
