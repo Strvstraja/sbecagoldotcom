2f8cff85855154acc8466160eef4874f2d620448
Strahinja Becagol <becagolwork@gmail.com>
2025-07-26 21:48:17 +0200

    ci: configure deployment settings and optimize build caching for GitHub Pages

--- sbecagoldotcom/astro.config.mjs
+++ sbecagoldotcom/astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://sbecagol.com',
+  trailingSlash: 'always',
   output: 'static',
   build: {
     outDir: './dist',
+    assets: '_assets'
   },
 });
