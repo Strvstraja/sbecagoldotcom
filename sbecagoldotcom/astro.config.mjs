b58e9d4bc299f987b083ada6f644cfc0db6181a2
Strahinja Becagol <becagolwork@gmail.com>
2025-07-26 09:22:53 +0200

    ci: streamline GitHub Pages deployment workflow and update Astro config structure

--- sbecagoldotcom/astro.config.mjs
+++ sbecagoldotcom/astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   site: 'https://strvstraja.github.io',
   base: '/sbecagoldotcom',
-  output: 'static',
   build: {
     assets: 'assets'
   }
