b0491fc93fad30aa5dcbf8dca09f4bcc13da27b9
Strahinja Becagol <becagolwork@gmail.com>
2025-08-09 10:14:25 +0200

    feat: add sitemap integration and robots.txt for improved SEO

--- sbecagoldotcom/astro.config.mjs
+++ sbecagoldotcom/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sbecagol.com',
   trailingSlash: 'never',
   output: 'static',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   build: {
     outDir: './dist',
     assets: '_assets'
